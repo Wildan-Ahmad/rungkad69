b948fa1c4b9d3659f787b331af3d4fbc027171ef
Wildan-Ahmad <85280377+Wildan-Ahmad@users.noreply.github.com>
2023-10-08 22:00:13 +0700

    Update rungkad.js

--- rungkad.js
+++ rungkad.js
@@ -6,50 +6,55 @@ var saldo = parseInt(d);
 
 while (i) {
   if (saldo > 0) {
+    //tentukan saldo awalmu
     alert(`Saldo yang anda punya Rp.${saldo}`);
     var b = prompt(`Masukkan Bet Anda:`);
     var bet = parseInt(b);
-    saldo = saldo - bet;
 
-    var t = 4;
+    if (saldo >= bet) {
+      saldo = saldo - bet;
+      var t = 4;
 
-    alert(`Tebak angka 1 - 10 \nAnda punya ${t - 1} kesempatan!!`);
-    var comp = Math.floor(Math.random() * 10) + 1;
+      //mengambil angka dari komputer
+      alert(`Tebak angka 1 - 10 \nAnda punya ${t - 1} kesempatan!!`);
+      var comp = Math.floor(Math.random() * 10) + 1;
 
-    //pengulangan per ronde
-    while (t > 1) {
-      //user memilih angka
-      var p = prompt(`Masukkan angka tebakan:`);
-      var inputNumb = parseInt(p);
+      //pengulangan per ronde
+      while (t > 1) {
+        //user memilih angka
+        var p = prompt(`Masukkan angka tebakan:`);
+        var inputNumb = parseInt(p);
 
-      var hasil = ``;
+        var hasil = ``;
 
-      //rule permainan
-      if (inputNumb == comp) {
-        saldo = saldo + bet * 2;
-        hasil = `Anda benar \nangka yang dicari adalah ${comp}`;
-        t = 0;
-      } else if (inputNumb > comp) {
-        t = t - 1;
-        hasil = `Angka terlalu Tinggi \nmasih ada ${t - 1} kesempatan`;
-      } else if (inputNumb < comp) {
-        t = t - 1;
-        hasil = `Angka terlalu Rendah \nmasih ada ${t - 1} kesempatan`;
-      } else {
-        alert(`Angka yang dimasukkan salah`);
-        t = 0;
+        //rule permainan
+        if (inputNumb == comp) {
+          saldo = saldo + bet * 2;
+          hasil = `Anda benar \nangka yang dicari adalah ${comp}`;
+          t = 0;
+        } else if (inputNumb > comp) {
+          t = t - 1;
+          hasil = `Angka terlalu Tinggi \nmasih ada ${t - 1} kesempatan`;
+        } else if (inputNumb < comp) {
+          t = t - 1;
+          hasil = `Angka terlalu Rendah \nmasih ada ${t - 1} kesempatan`;
+        } else {
+          alert(`Angka yang dimasukkan salah`);
+          t = 0;
+        }
+        //menampilkan hasil permainan
+        if (t == 1) {
+          alert(`Anda gagal menjawab \nangka yang dicari adalah ${comp}`);
+        } else {
+          alert(hasil);
+        }
       }
-
-      //menampilkan hasil permainan
-      if (t == 1) {
-        alert(`Anda gagal menjawab \nangka yang dicari adalah ${comp}`);
-      } else {
-        alert(hasil);
-      }
+    } else {
+      alert(`Saldo Anda tidak mencukupi harap depo`);
+      var i = confirm(`Mau main lagi?`);
     }
-    var i = confirm(`Mau main lagi?`);
   } else {
-    alert(`Saldo anda tidak mencukupi harap depo`);
+    alert(`Saldo Kamu habis. \njangan menyerah masih ada esok hari!!`);
     i = false;
   }
 }
